5d5080c51ab2afa2a97d2ea1f723affc23f1a426
MisterJaJo <jarvistrigo@gmail.com>
2022-02-24 03:46:59 +0100

    dwmsb blocks work now

--- dwmsb/config.mk
+++ dwmsb/config.mk
@@ -6,10 +6,17 @@ VERSION = 0.1
 # Paths
 PREFIX = /usr/local
 
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# Includes and Libs
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11
+
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
-LDFLAGS  = 
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  = ${LIBS}
 
 # Compiler and Linker
 CC = cc
