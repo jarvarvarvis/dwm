3b90d807fad7e1b74470267afabb035e9ef0dce8
MisterJaJo <jarvistrigo@gmail.com>
2022-02-25 17:17:50 +0100

    Added configurable padding for bar status text

--- config.h
+++ config.h
@@ -7,6 +7,8 @@ static const unsigned int gappx     = 15;       /* gap size between windows in p
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int statusrpad         = 6;        /* right padding of bar status text in px */
+static const int statuslpad         = 6;        /* left padding of bar status text in px */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
--- dwm.c
+++ dwm.c
@@ -742,7 +742,7 @@ drawbar(Monitor *m)
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding on status text */
+		tw = TEXTW(stext) - lrpad + statusrpad; /* right padding on status text */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
 	}
 
@@ -769,7 +769,7 @@ drawbar(Monitor *m)
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w - 2,  /* 2px left padding on status text */
+			drw_text(drw, x, 0, w - statuslpad,  /* left padding on status text */
 			         bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
