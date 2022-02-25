e7f6ffdb041089c519142944e75e907afb51ffbb
MisterJaJo <jarvistrigo@gmail.com>
2022-02-25 13:31:46 +0100

    Removed memory block in status bar

--- dwmsb/config.h
+++ dwmsb/config.h
@@ -10,27 +10,10 @@ char *datecmd()
 	return s;
 }
 
-char *memcmd()
-{
-	// Get sysinfo
-	struct sysinfo info;
-	sysinfo(&info);
-
-	// Create string
-	unsigned long totalram = info.totalram;
-	unsigned long freeram  = info.freeram;
-	unsigned long usedram  = totalram - freeram;
-
-	char *s = malloc(sizeof(char) * 64);
-	sprintf(s, "%ld / %ld", usedram, totalram);
-	return s;
-}
-
 /* Definitions of blocks */
 static const Block blocks[] =
 {
 	{ "Date: ", &datecmd },
-	{ "Mem: ",  &memcmd  }
 };
 
 /* The update interval in seconds */
