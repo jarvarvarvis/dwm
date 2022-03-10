0f0019e46da0f77ed5e6ef319d0b440808df95d0
MisterJaJo <jarvistrigo@gmail.com>
2022-03-10 21:28:00 +0100

    Proper status text left and right padding

--- dwm.c
+++ dwm.c
@@ -744,6 +744,11 @@ drawbar(Monitor *m)
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + statusrpad; /* right padding on status text */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
+		tw += statuslpad; /* left padding on status text */
+
+		/* Draw status left padding rectangle */
+		drw_setscheme(drw, scheme[SchemeNorm]);
+		drw_rect(drw, m->ww - tw, 0, statuslpad, bh, 1, 1);
 	}
 
 	for (c = m->clients; c; c = c->next) {
@@ -769,8 +774,7 @@ drawbar(Monitor *m)
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w - statuslpad,  /* left padding on status text */
-			         bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
