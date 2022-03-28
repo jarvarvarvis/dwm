ac18e90ddfa3dc85517e329797b1edb745b34021
MisterJaJo <jarvistrigo@gmail.com>
2022-03-29 00:08:02 +0200

    .desktop files in ~/.config/autostart/ are now autostarted

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Start feh (desktop wallpaper)
 feh --bg-scale ~/dwm/wallpaper.png
@@ -8,6 +8,12 @@ setxkbmap de
 
 # Start picom
 picom --config ~/dwm/picom.conf &
+
+# Start .desktop files in ~/.config/autostart/
+# This code depends on the program dex - if you use Arch, just sudo pacman -S dex
+for file in ~/.config/autostart/*; do
+	dex $file &
+done
 
 # Kill previous instances of dwmsb and then start it again
 killall dwmsb
